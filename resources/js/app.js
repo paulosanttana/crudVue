7de6624948221f86379211f6ffb44e1d6281ea00
PauloRSantana <paulorobsantana@gmail.com>
2020-01-03 15:47:04 -0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,45 @@
+// app.js
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+import HomeComponent from './components/Post/HomeComponent.vue';
+import CreateComponent from './components/Post/CreateComponent.vue';
+import IndexComponent from './components/Post/IndexComponent.vue';
+import EditComponent from './components/Post/EditComponent.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: CreateComponent
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: IndexComponent
+  },
+  {
+      name: 'edit',
+      path: '/edit/:id',
+      component: EditComponent
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
